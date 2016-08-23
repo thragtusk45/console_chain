b6df0ed5c8e386171d954deb8773bf93a11a2d67
alexey <amezhuev@gmail.com>
2016-08-23 14:40:49 +0300

    tests

--- src/Oro/ChainCommandBundle/Tests/Functional/CommandChainTest/CommandChainTest.php
+++ src/Oro/ChainCommandBundle/Tests/Functional/CommandChainTest/CommandChainTest.php
@@ -2,51 +2,33 @@
 
 namespace Oro\ChainCommandBundle\Tests\Functional\CommandChainTest;
 
+
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
-use Symfony\Component\Console\Application;
-use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\Console\Output\BufferedOutput;
-use Symfony\Component\Console\Tester\ApplicationTester;
 
 /**
  * Class CommandChainTest
+ * Tried to do this via ApplicationTester for 3 days, then i just gave up
  * @package ConsoleCommandChainBundle\Tests\Functional\CommandChainTest
  * @author Alexey Mezhuev <a.mezhuev@bikeportal.in>
  */
 class CommandChainTest extends KernelTestCase
 {
-
     /**
      * Tests
      * @throws \Exception
      */
     public function testPositive()
     {
-        static::bootKernel();
-        $application = new Application(static::$kernel);
-        $application->setAutoExit(false);
-
-        $tester = new ApplicationTester($application);
-
-        $command = ['command' => 'foo:hello'];
-        $tester->run($command);
-        $output = $tester->getDisplay();
-
-
+        exec('php app/console foo:hello', $output);
+        $output = implode(' ',$output);
         $this->assertRegExp('/Hello from Foo/', $output);
         $this->assertRegExp('/Hi from Bar/', $output);
     }
 
     public function testNegative()
     {
-        static::bootKernel();
-        $application = new Application(static::$kernel);
-        $application->setAutoExit(false);
-
-        $tester = new ApplicationTester($application);
-        $command = ['command' => 'bar:hi'];
-        $tester->run($command);
-        $output = $tester->getDisplay();
+        exec('php app/console bar:hi 2>&1', $output);
+        $output = implode(' ',$output);
 
         $this->assertRegExp('/bar:hi command is a member of foo:hello command chain and cannot be executed on its own./', $output);
     }
